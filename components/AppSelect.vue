eef7b8ab4af861c4da2122830bd7d5b3c5b325fe
ayhan basmisirli <ayhan.basmisirli@gmail.com>
2020-10-08 23:27:47 +0300

    nuxtfood

---
+++ components/AppSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label :for="select">What {{ select }} do you want</label>
+    <select :id="select" :name="select" @change="$emit('change',$event.target.value)">
+      <option value="">
+        Please select a {{ select }}
+      </option>
+      <option v-for="option in selectOptions" :key="option">
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    select: {
+      type: String,
+      default: 'cuisine'
+    },
+    selectOptions: {
+      type: Array,
+      default: () => ['taco', 'pizza', 'dim sum']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,16 +1,43 @@
 <template>
   <main class="container restaurant">
-    <h1>Restaurants</h1>
-    <AppRestaurantInfo />
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+      <AppSelect @change="selectedRestaurant = $event" />
+    </div>
+
+    <AppRestaurantInfo :datasource="filteredRestaurants" />
   </main>
 </template>
 
 <script>
 import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue'
+import AppSelect from '@/components/AppSelect.vue'
+import { mapState } from 'vuex'
 
 export default {
   components: {
-    AppRestaurantInfo
+    AppRestaurantInfo,
+    AppSelect
+  },
+  data () {
+    return {
+      selectedRestaurant: ''
+    }
+  },
+  computed: {
+    ...mapState([
+      'fooddata'
+    ]),
+    filteredRestaurants () {
+      if (this.selectedRestaurant) {
+        return this.fooddata.filter((el) => {
+          const rname = el.name.toLowerCase()
+          return rname.includes(this.selectedRestaurant)
+        })
+      }
+      return this.fooddata
+    }
+
   }
 }
 
